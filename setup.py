c15a0074ed27d79527f4c0dc746c9005636467f9
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2014-11-24 11:47:11 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGELOG = open(os.path.join(here, 'CHANGELOG.txt')).read()
 
-version = '0.0.1'
+version = '0.0.2'
 
 setup(name='crazyhash',
       version=version,
